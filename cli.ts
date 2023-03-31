1da618585602e09292e2c3ef7d997711be57cbbb
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-31 21:03:42 +0200

    update cli with push-to-git data

--- cli.ts
+++ cli.ts
@@ -1,7 +1,11 @@
 import { Command } from "./deps/cliffy.ts";
+import { VERSION } from "./VERSION.ts";
 
 await new Command()
-  .name("cliffy")
-  .version("0.1.0")
-  .description("Command line framework for Deno")
+  .name("push-to-git")
+  .version(VERSION)
+  .description(`
+    Pushes the current branch to an arbitrary branch 
+    in the same or in another repository for deployment.
+  `)
   .parse(Deno.args);
