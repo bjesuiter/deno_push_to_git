0faef111e21b5ea8eb29ed500131978ba04e0e0f
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-31 22:24:27 +0200

    extend subproc example

--- example/subproc.ts
+++ example/subproc.ts
@@ -1,5 +1,15 @@
+const utf8 = new TextDecoder();
+
 const cmd = new Deno.Command("git", {
-    args: ['-v']
+  args: ["-v"],
 });
 
-cmd.spawn();
+const cmdOut1 = await cmd.output();
+console.log(cmdOut1, utf8.decode(cmdOut1.stdout), utf8.decode(cmdOut1.stderr));
+
+const currBranchCommand = new Deno.Command("git", {
+  args: ["rev-parse --abbrev-ref HEAD"],
+});
+
+const cmdOut2 = await currBranchCommand.output();
+console.log(cmdOut2, utf8.decode(cmdOut2.stdout), utf8.decode(cmdOut2.stderr));
