983105898f4ca24497ea51138ff5b63330782229
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-31 22:31:49 +0200

    cleanup repo

--- mod.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Export all functionality of your module here,
- * which should be used by other people
- */
-
-export * from "./lib/dummy.ts"
--- test/mod.test.ts
+++
@@ -1,8 +0,0 @@
-import { assert, describe, it } from "std_testing";
-import { dummyExport } from "@/mod.ts";
-
-describe(`mod.ts`, () => {
-  it(`should export correct objects and types`, () => {
-    assert(dummyExport);
-  });
-});
