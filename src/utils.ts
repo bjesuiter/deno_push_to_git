b67438a6d41b277fd471a1520d73f4914842ac3d
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-03 21:16:36 +0200

    add custom exec function based on Deno.Command API

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+export async function exec(executable: string, args: string[]) {
+  const cmd = new Deno.Command(executable, {
+    stdout: "piped",
+    stderr: "piped",
+    args,
+  });
+
+  const childProcess = cmd.spawn();
+  const cmdOut = await childProcess.output();
+  const stdout = new TextDecoder().decode(cmdOut.stdout).trim();
+  const stderr = new TextDecoder().decode(cmdOut.stderr).trim();
+  return `
+  stdout: ${stdout}
+  stderr: ${stderr}
+  `;
+}
